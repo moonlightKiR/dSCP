ead0f5762cc4a693515e3c5871cd1b471b4380bd
moonlightKiR <81652246+moonlightKiR@users.noreply.github.com>
2026-03-22 11:12:28 +0100

    moved main.py into app folder

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-from app.database import kaggle_retreiver
+from database import kaggle_retreiver
+
 
 def main():
     database = kaggle_retreiver.KaggleRetreiver()
     database.download_data()
+
 
 if __name__ == "__main__":
     main()
